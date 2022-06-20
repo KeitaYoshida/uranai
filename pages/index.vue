2eee64752ea820f36875dd471c7159a8b6894b33
KeitaYoshida <ba0503naky@gmail.com>
2022-06-20 17:02:36 +0900

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <Header :config="config" />
+    <TheHero />
+    <Footer />
+  </div>
+</template>
+
+<script>
+export default {
+  data: (vm) => ({
+    config: {
+      siteName: "サイト名",
+    },
+  }),
+};
+</script>
