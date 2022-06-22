495f862f14bade207a09575de9c2c6d87a5805da
KeitaYoshida <ba0503naky@gmail.com>
2022-06-22 16:32:23 +0900

    up top

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,7 @@ import { defineNuxtConfig } from "nuxt";
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   buildModules: ["nuxt-windicss"],
+  router: {
+    scrollBehavior: () => ({ x: 0, y: 0 }),
+  },
 });
